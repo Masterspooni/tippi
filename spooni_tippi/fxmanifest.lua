9c004965a9a88f581fc9ec151d96f8d3c923c44e
Masterspooni <127257627+Masterspooni@users.noreply.github.com>
2023-06-15 08:25:23 +0200

    Add files via upload
    Welcome to Spooni Free Stuff for RDR2. 
    ---------------------------------------
    We actually create any prop or houses or Mlo´s you want. 
    
    Ask us on Discord https://discord.gg/byASpSfwuk.
    -------------------------------------------------------
    
    How is it installed?
    
    Copy spooni_tippi into the resources folder. 
    
    Then in the server.cfg or resources.cfg
    write
    
    ensure spooni_tippi
    
    
    Restart Server.
    
    Start Spooner or use the Free spooni_spooner
    
    If you have the normal Spooner open Objects List and typ in the top the name tippi3
    
    than down spwan by name.
    
    Press E wait a moment and the Tippi spwan.
    
    ---------------------------------------------------------------------------
    
    Download the new Spooni Spooner here to easy use the Props Houses and Stables
    
    https://github.com/i3ucky/spooni_spooner 
    
    then restart server. 
    
    Start Spooner under Spooni, you see the list of Spooni Houses, Stables and Props is now at the top. 
    
    It is important that all objects are in it, but of course only those that you have purchased will work.
    ----------------------------------------------------------------------------------------------------
    
    You need Help for Ymaps or XML´s 
    or you need to know how creat a doorhash for all doors.
    
    Instruction here:
    https://discord.gg/fYwkxpQSSU
    
    
    have fun.

---
+++ spooni_tippi/fxmanifest.lua
@@ -0,0 +1,16 @@
+fx_version "adamant"
+author 'Spooni-Mapping'
+game "rdr3"
+
+rdr3_warning "I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships."
+this_is_a_map "yes"
+lua54 'yes'
+
+
+  files {
+   'stream/*.ytyp'
+
+  } 
+  
+  
+  data_file 'DLC_ITYP_REQUEST' 'stream/*.ytyp'
